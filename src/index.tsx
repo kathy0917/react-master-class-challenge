b6156225417e1aae6d4be599587b0a34785f2b39
Nahyeon Kang <kathy95917@naver.com>
2024-10-05 01:29:41 +0900

    feat: router 설정 및 globalstyle 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { theme } from './theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
