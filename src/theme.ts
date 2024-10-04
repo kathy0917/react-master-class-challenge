5fbe61bb7336733dcd6a04c48c103bf8bcf0c5e2
Nahyeon Kang <kathy95917@naver.com>
2024-10-05 03:16:58 +0900

    feat: paprica api 호출 및 loading 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  bgColor: 'white',
-  textColor: 'black',
-  accentColor: 'tomato',
+  bgColor: '#2E363F',
+  textColor: 'white',
+  accentColor: '#8784C9',
 };
